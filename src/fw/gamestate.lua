9e343d040d84c949fced4e312b1924eacb8c59f2
Jarl Erik Schmidt <github@jarlerik.com>
2012-11-25 21:39:52 +0100

    Added missing gamestate.lua file

---
+++ src/fw/gamestate.lua
@@ -0,0 +1,58 @@
+local M = {}
+
+M.State = require "fw.gamestate.State"
+M.TransitionState = require "fw.gamestate.TransitionState"
+
+function M.newLevel()
+
+   local level = State.new()
+
+   function level:new(o)
+      o = o or {}
+      setmetatable(o, level)
+      self.__index = self
+
+      return o
+   end
+
+   function level:load()
+   end
+
+   return level
+
+end
+
+function M.newState(o)
+   return M.State.new(o)
+end
+
+--- Wrapper for TransitionState constructor
+-- @param old The old state to transition away from (can be null)
+-- @param new The new state to transition into (cannot be null!)
+-- @param duration (optional) Transition time in seconds
+function M.transition(old, new, duration)
+   return M.TransitionState.new(old, new, duration)
+end
+
+--- Wrapper for TransitionState constructor with nil as old state
+-- @param s The state to transition into
+-- @param duration (optional) Transition time in seconds
+function M.fadeIn(s, duration)
+   return M.TransitionState.new(nil, s, duration)
+end
+
+function M.loadLevel(file)
+
+   local f, level = require(file), nil
+
+   assert(type(f) == "table" and type(f["new"]) == "function",
+	  "Expected '" .. file .. "' to return a State 'class'")
+
+   level = f:new()
+   level:load()
+
+   return level
+
+end
+
+return M
