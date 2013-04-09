3d418fe3365d09fe151213befb059b5665fd43bc
Jarl Erik Schmidt <github@jarlerik.com>
2013-04-10 00:22:36 +0200

    Added existance check for ldoc repository, generate docs after clone

--- configure.sh
+++ configure.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# Clone ldoc for documentation generation
-git clone https://github.com/stevedonovan/LDoc.git external/ldoc
+# If the ldoc project hasn't been checked out:
+if [ ! -d "external/ldoc" ]; then
+    # Clone ldoc for documentation generation
+    git clone https://github.com/stevedonovan/LDoc.git external/ldoc
+fi
+
+# Generate docs for the first time
+lua external/ldoc/ldoc.lua src
