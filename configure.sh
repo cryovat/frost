22301a3f074233184e02fd0f3ca17141a4d38e69
Jarl Erik Schmidt <github@jarlerik.com>
2013-04-10 02:13:14 +0200

    Removed configure.sh

--- configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# If the ldoc project hasn't been checked out:
-if [ ! -d "external/ldoc" ]; then
-    # Clone ldoc for documentation generation
-    git clone https://github.com/stevedonovan/LDoc.git external/ldoc
-fi
-
